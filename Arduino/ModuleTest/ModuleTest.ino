3eb4eab91dd91585195da61fb1d30049073319ec
Kaan Inan <kaaninan99@gmail.com>
2016-02-16 19:21:36 +0000

    arduino modules added

---
+++ Arduino/ModuleTest/ModuleTest.ino
@@ -0,0 +1,9 @@
+int lazer = 4;
+
+void setup(){
+  pinMode(lazer, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(lazer, 1);
+}
--- ROS/build/catkin_generated/setup_cached.sh
+++ ROS/build/catkin_generated/setup_cached.sh
@@ -7,6 +7,6 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/home/rock/ProjectX/ROS/devel/lib/arm-linux-gnueabihf:/home/rock/catkin-ws/devel/lib/arm-linux-gnueabihf:/opt/ros/indigo/lib/arm-linux-gnueabihf:/home/rock/ProjectX/ROS/devel/lib:/home/rock/catkin-ws/devel/lib:/opt/ros/indigo/lib"
-export PKG_CONFIG_PATH="/home/rock/ProjectX/ROS/devel/lib/arm-linux-gnueabihf/pkgconfig:/home/rock/catkin-ws/devel/lib/arm-linux-gnueabihf/pkgconfig:/opt/ros/indigo/lib/arm-linux-gnueabihf/pkgconfig:/home/rock/ProjectX/ROS/devel/lib/pkgconfig:/home/rock/catkin-ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export LD_LIBRARY_PATH="/home/rock/ProjectX/ROS/devel/lib/arm-linux-gnueabihf:/opt/ros/indigo/lib/arm-linux-gnueabihf:/home/rock/ProjectX/ROS/devel/lib:/opt/ros/indigo/lib"
+export PKG_CONFIG_PATH="/home/rock/ProjectX/ROS/devel/lib/arm-linux-gnueabihf/pkgconfig:/opt/ros/indigo/lib/arm-linux-gnueabihf/pkgconfig:/home/rock/ProjectX/ROS/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
 export PWD="/home/rock/ProjectX/ROS/build"

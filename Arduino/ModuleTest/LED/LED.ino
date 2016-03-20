3f12e6c74f174dc97bf9ecdf9b31b5fa372514c1
Kaan INAN <kaaninan99@gmail.com>
2016-03-20 23:05:15 +0200

    last update tubitak

---
+++ Arduino/ModuleTest/LED/LED.ino
@@ -0,0 +1,24 @@
+int pin = 9;
+
+void setup() {
+  pinMode(pin, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  analogWrite(6, 20);
+  analogWrite(4, 10);
+  analogWrite(pin, 200);
+
+//  digitalWrite(8, 1);
+//  digitalWrite(11, 1);
+//  digitalWrite(12, 1);
+
+  analogWrite(8,  253);
+  analogWrite(11, 250);
+  analogWrite(12, 250);
+}
